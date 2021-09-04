424664f2da99fea8a66ada7b416cd017f8e75805
Jayesh Choudhari <choudhari.jayesh@alumni.iitgn.ac.in>
2021-09-04 23:51:37 +0100

    This directory is present under HMHPModel

--- include/initialization.h
+++
@@ -1,9 +0,0 @@
-#ifndef INITIALIZATION_H 
-#define INITIALIZATION_H
-
-vector< vector <li> > initializeForSampler(vector< vector <li> > allEvents, int flag);
-int initializeUserUserInfluence();
-int initializeAvgProbabilityVectors();
-int initializeAvgTopicProbabilityVectors();
-
-#endif
--- include/utilities.h
+++
@@ -1,6 +0,0 @@
-#ifndef UTILITIES_H 
-#define UTILITIES_H
-
-
-
-#endif
